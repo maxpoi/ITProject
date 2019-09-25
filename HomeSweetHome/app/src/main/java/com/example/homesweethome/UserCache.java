55e28f3026a63744dbd54289e869fbf938dc0c8b
YE JIACHENG <misaki.cy@gmail.com>
2019-09-26 02:25:09 +1000

    Now is able to save data to local, read data locally. Speed up the performance. But logic has some bad effects, will be solved in next patch.

--- HomeSweetHome/app/src/main/java/com/example/homesweethome/UserCache.java
+++ HomeSweetHome/app/src/main/java/com/example/homesweethome/UserCache.java
@@ -1,5 +1,8 @@
 package com.example.homesweethome;
 
+import android.content.Context;
+
+import java.io.FileOutputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -7,13 +10,13 @@ public class UserCache {
     private static final UserCache ourInstance = new UserCache();
     private ArrayList<Cell> cells;
 
-    static UserCache getInstance() {
-        return ourInstance;
-    }
+    static UserCache getInstance() { return ourInstance; }
 
     private UserCache() {}
 
     public void setCells(ArrayList<Cell> cells) { this.cells = cells; }
+
+    public void initialize() { this.cells = new ArrayList<>(); }
 
     public ArrayList<Cell> getCells() { return this.cells; }
 
@@ -29,4 +32,5 @@ public class UserCache {
     public Cell getCell(int position) { return cells.get(position); }
 
     public ArrayList<Image> getImagesByCell(int position) { return getCell(position).getImages(); }
+
 }
